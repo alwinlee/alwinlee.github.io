0f1e149aaea0515c57e0525fcd085970d42fcd5e
Alwin <alwinlee0915@gmail.com>
2019-01-28 11:40:12 +0800

    update js file and function

---
+++ static/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('#show-my-info').on('click', function() {
+        $('#items-1').html('Hi, you don\'t know me at all.');
+        alert('Hi, It\'s me!!!');
+    });
+
+
+});
